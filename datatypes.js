b3396018e016eb1c0456d58539206b7c09d8cb0f
mrhrifat <mrhrifat383759@gmail.com>
2021-03-31 04:58:45 +0600

    Add datatypes

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+var i = 12;
+var i2 = 4;
+var d = 3.5;
+var d2 = 4.5;
+var s = "Hackerrank";
+var s2 = " is the best website in the world!";
+console.log(i + i2); console.log(d + d2); console.log(s + s2);
+
+
+console.log();
+// Function
+// function processData(one, two, three) {
+    one=parseInt(readLine());
+    two=parseFloat(readLine());
+    three=readLine();
+    console.log(i + one);
+    console.log(d + two);
+    console.log(s + three);
+// }
+// processData(12, 3.5.toFixed(1), " is best place to code!") 
